71b4223b3ea2c95aba0ba18c77a7a97a55c300e8
Carlton Joseph <carlton.joseph@gmail.com>
2023-05-24 16:45:01 -0700

    add basic syntax

--- rust003_basicsyntax/src/main.rs
+++ rust003_basicsyntax/src/main.rs
@@ -8,4 +8,121 @@ fn main() {
     let mut a: [i8; 10] = [42; 10];
     a[5] = 0;
     println!("a: {:?}", a);
+
+    let t: (i8, bool) = (7, true);
+    println!("1st index: {}", t.0);
+    println!("2nd index: {}", t.1);
+
+    let mut x: i32 = 10;
+    let ref_x: &mut i32 = &mut x;
+    println!("x count ones: {}", ref_x.count_ones());
+    *ref_x = 20;
+    println!("x: {x}");
+
+    /*
+    {
+        // dangling references statically forbidden
+        let ref_x: &i32;
+        {
+            let x: i32 = 10;
+            ref_x = &x;
+        }
+        println!("ref_x: {ref_x}");
+    }
+    */
+
+    let mut a: [i32; 6] = [10, 20, 30, 40, 50, 60];
+    println!("a: {a:?}");
+
+    let s: &[i32] = &a[2..4];
+    println!("s: {s:?}");
+    // a[3] = 35; // will fail since slice alread borrowed it
+    a[4] = 55;
+    println!("a: {a:?}");
+
+    let s1: &str = "World";
+    println!("s1: {s1}");
+
+    let mut s2: String = String::from("Hello ");
+    println!("s2: {s2}");
+    s2.push_str(s1);
+    println!("s2: {s2}");
+
+    let s3: &str = &s2[6..];
+    println!("s3: {s3}");
+
+    print_fizzbuzz_to(20);
+
+    let mut rect = Rectangle {
+        width: 10,
+        height: 5,
+    };
+    println!("old area: {}", rect.area());
+    rect.inc_width(5);
+    println!("new area: {}", rect.area());
+    println!("new area: {:?}", Rectangle::new(3, 4).area());
+    println!("new area: {:?}", Rectangle::new_square(3).area());
+
+    println!("coin toss: {}", pick_one("heads", "tails"));
+    println!("cash prize: {}", pick_one(500, 1000));
+}
+
+/// Determine whether the first argument is divisible by the second argument.
+///
+/// If the second argument is zero, the result is false.
+fn is_divisible(n: u32, divisor: u32) -> bool {
+    if divisor == 0 {
+        return false;
+    }
+    n % divisor == 0
+}
+
+fn fizzbuzz(n: u32) -> String {
+    let fizz = if is_divisible(n, 3) { "fizz" } else { "" };
+    let buzz = if is_divisible(n, 5) { "buzz" } else { "" };
+    if fizz.is_empty() && buzz.is_empty() {
+        return format!("{n}");
+    }
+    format!("{fizz}{buzz}")
+}
+
+fn print_fizzbuzz_to(n: u32) {
+    for i in 1..=n {
+        println!("{}", fizzbuzz(i));
+    }
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn inc_width(&mut self, delta: u32) {
+        self.width += delta;
+    }
+
+    fn new(width: u32, height: u32) -> Rectangle {
+        Rectangle { width, height }
+    }
+
+    fn new_square(width: u32) -> Rectangle {
+        Rectangle {
+            width,
+            height: width,
+        }
+    }
+}
+
+fn pick_one<T>(a: T, b: T) -> T {
+    if std::process::id() % 2 == 0 {
+        a
+    } else {
+        b
+    }
 }
